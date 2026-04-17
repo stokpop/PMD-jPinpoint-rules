ef1f2024e369717b71e09f6b0fb9e34997254c6d
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2026-04-17 19:05:13 +0000

    Migrate Kotlin concurrent rules: AvoidUnguardedAssignmentInObjectsUsingSynchronized + AvoidUnguardedMutableFieldsInSharedObjects
    - AvoidUnguardedAssignmentToNonFinalFieldsInObjectsUsingSynchronized: 8 test cases
      Detects unguarded assignments in classes using @Synchronized or synchronized blocks
    - AvoidUnguardedMutableFieldsInSharedObjects: 13 test cases
      Detects var fields and known mutable types without proper guards in singletons

---
+++ src/test/java/com/jpinpoint/perf/lang/kotlin/ruleset/concurrent/AvoidUnguardedAssignmentToNonFinalFieldsInObjectsUsingSynchronizedTest.java
@@ -0,0 +1,6 @@
+package com.jpinpoint.perf.lang.kotlin.ruleset.concurrent;
+
+import net.sourceforge.pmd.test.PmdRuleTst;
+
+public class AvoidUnguardedAssignmentToNonFinalFieldsInObjectsUsingSynchronizedTest extends PmdRuleTst {
+}
