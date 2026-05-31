4d7e83b83375e0816ef63b7d1b0d8bb6ba8f0c27
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2026-05-31 19:41:44 +0000

    Port 7 Kotlin PMD performance rules batch 10

---
+++ src/test/java/com/jpinpoint/perf/lang/kotlin/ruleset/remoting/HttpInterceptorNotReleasingOnExceptionTest.java
@@ -0,0 +1,6 @@
+package com.jpinpoint.perf.lang.kotlin.ruleset.remoting;
+
+import com.jpinpoint.perf.lang.kotlin.KotlinRuleTest;
+
+public class HttpInterceptorNotReleasingOnExceptionTest extends KotlinRuleTest {
+}
