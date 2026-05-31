941aed257b2a730c4ee46609461c451adf479cc7
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2026-05-31 20:22:21 +0000

    Port final 3 Kotlin PMD performance rules

---
+++ src/test/java/com/jpinpoint/perf/lang/kotlin/ruleset/remoting/HttpClientImproperConnectionTimeoutsTest.java
@@ -0,0 +1,6 @@
+package com.jpinpoint.perf.lang.kotlin.ruleset.remoting;
+
+import com.jpinpoint.perf.lang.kotlin.KotlinRuleTest;
+
+public class HttpClientImproperConnectionTimeoutsTest extends KotlinRuleTest {
+}
